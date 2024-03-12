c80b2d0a45247dbf98757e77f9f1182d1ce481bd
Aman07a <aman16.aa17@gmail.com>
2024-03-12 14:54:30 +0100

    Exercise 2

---
+++ exercises/exercise-2.py
@@ -0,0 +1,17 @@
+# Exercise 2
+weight = float(input("Weight: "))
+unit = input("(K)g or (L)bs: ")
+
+if unit.upper() == "K":
+    converted = weight / 0.45
+    print("Weight in Lbs: " + str(converted))
+else:
+    converted = weight * 0.45
+    print("Weight in Kgs: " + str(converted))
+
+"""
+Output:
+Weight:
+(K)g or (L)bs:
+Weight in Kg:
+"""
