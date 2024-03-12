aec79f274f43cd755bb7dfd0da915340911a6c9d
Aman07a <aman16.aa17@gmail.com>
2024-03-12 14:59:56 +0100

    While Loops

--- app.py
+++ app.py
@@ -1,13 +1,9 @@
-# If Statements
-temperature = 5
+# While Loops
+i = 1
+while i <= 5:
+    print(i)
+    i = i + 1
 
-if temperature > 30:
-    print("It's a hot day")
-    print("Drink plenty of water")
-elif temperature > 20:
-    print("It's a nice day")
-elif temperature > 10:
-    print("It's a bit cold")
-else:
-    print("It's cold")
-print("Done")
+while i <= 10:
+    print(i * '*')
+    i = i + 1
