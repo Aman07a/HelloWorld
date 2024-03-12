e814f8ce8eb1e3652584e9e4e41050f8b67bf34d
Aman07a <aman16.aa17@gmail.com>
2024-03-12 13:53:40 +0100

    Type Conversion

--- app.py
+++ app.py
@@ -1,2 +1,9 @@
-name = input("What is your name? ")
-print("Hello " + name)
+# Type Conversion
+birth_year = input("Enter your birth year: ")
+age = 2020 - int(birth_year)
+print(age)
+
+first = float(input("First: "))
+second = float(input("Second: "))
+sum = float(first) + float(second)
+print("Sum: " + str(sum))
