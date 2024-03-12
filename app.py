df738e3dea39a0a8443675fc9c8ddd6fee3663e2
Aman07a <aman16.aa17@gmail.com>
2024-03-12 14:25:17 +0100

    Arithmetic Operators

--- app.py
+++ app.py
@@ -1,5 +1,9 @@
-# Strings
-course = "Python for Beginners"
-print(course.find('y'))
-print(course.replace('x', '4'))
-print('Python' in course)
+# Arithmetic Operators
+print(10 / 3)
+print(10 * 3)
+print(10 ** 3)
+
+x = 10
+x = x + 3
+x *= 3
+print(x)
