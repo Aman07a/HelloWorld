278f2f0b4a3d62d1699062681aa7a2b9f621c129
Aman07a <aman16.aa17@gmail.com>
2024-03-12 12:21:06 +0100

    Exercise 1

---
+++ exercise.py
@@ -0,0 +1,15 @@
+# Exercise 1
+patient_name = "John Smith"
+age = 20
+patient_type = "new"
+
+print("We check in a patient named", patient_name + ".")
+print("He is", age, "years old.")
+print("He is a", patient_type, "patient.")
+
+"""
+Output:
+We check in a patient named John Smith.
+He is 20 years old.
+He is a new patient.
+"""
